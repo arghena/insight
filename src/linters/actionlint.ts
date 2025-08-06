9ea19ff8f9c7cb85fe86f2dc5abac8285eb8e5bb
Shigure Kurosaki <shigure@hqsy.net>
2025-08-06 11:45:26 +0800

    fix(go): properly set up the Go environment
    Signed-off-by: Shigure Kurosaki <shigure@hqsy.net>

--- src/linters/actionlint.ts
+++ src/linters/actionlint.ts
@@ -13,5 +13,5 @@ export async function runner(
 
     info(`[runner] Checking ${paths.length} files with ${name}`)
 
-    await exec(`$(go env GOPATH)/bin/${name}`, [...options, ...paths])
+    await exec(`sh -c $(go env GOPATH)/bin/${name}`, [...options, ...paths])
 }
