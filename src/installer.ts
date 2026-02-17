7b37fabb26e4b434138b0f641fd95e33cebf3ec9
Shigure Kurosaki <shigure@hqsy.net>
2026-02-18 01:27:45 +0800

    fix: bug
    Signed-off-by: Shigure Kurosaki <shigure@hqsy.net>

--- src/installer.ts
+++ src/installer.ts
@@ -23,9 +23,7 @@ const installedTools = new Set<PackageManager | ToolName>()
 // `cargo-binstall` and `uv` doesn't actually work.
 export async function installer(toolName: ToolName, version: string): Promise<void> {
     const setupMap = {
-        pnpm: [
-            'curl -fsSL https://raw.githubusercontent.com/pnpm/get.pnpm.io/main/install.sh | sh',
-        ],
+        pnpm: ['corepack enable pnpm'],
         rustup: [
             `rustup toolchain install ${version === 'latest' ? 'stable' : version} --profile minimal --no-self-update`,
             `rustup override set ${version === 'latest' ? 'stable' : version}`,
